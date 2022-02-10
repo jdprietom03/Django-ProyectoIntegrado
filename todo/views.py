a53d3e4b4d40f8a516942fcb16652a11a1247c90
jdprietom03 <80794157+jdprietom03@users.noreply.github.com>
2022-02-10 14:28:12 +0000

    Initial commit

---
+++ todo/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from .models import TodoItem
+from django.views.decorators.csrf import csrf_protect
+
+# Create your views here.
+first_time = True
+
+# Create your views here.
+@csrf_protect
+def todoView(request):
+  global first_time
+  all_todo_items = TodoItem.objects.all()
+  if (first_time):
+      new_item = TodoItem(content = "build a cool app on replit.com")
+      new_item.save()
+      first_time = False
+  return render(request, 'index.html', 
+  {'all_items': all_todo_items})
+	
+@csrf_protect
+def addTodo(request):
+  new_item = TodoItem(content = request.POST['content'])
+  new_item.save()
+  return HttpResponseRedirect('/')
+
+@csrf_protect
+def deleteTodo(request, todo_id):
+  item_to_delete = TodoItem.objects.get(id=todo_id)
+  item_to_delete.delete()
+  return HttpResponseRedirect('/')
+
+@csrf_protect
+def coverImage(request):
+    image_data = open("./todo/app.png", "rb").read()
+    return HttpResponse(image_data, content_type="image/png")
